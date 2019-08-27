109c23a3dc12f6cc4e050bd09fa03e08de689bbb
Jacky Chiou <songjie1120@g.ncu.edu.tw>
2019-08-27 12:25:40 +0800

    setup and home page

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app dark>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    //
+  })
+};
+</script>
+<style>
+#app {
+  background-color: gray;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col class="col-sm-6 col-12">
+        <v-card>
+          <v-img
+            src="../assets/pic/ceiling-chairs-coffee-shop-1863622-2.jpg"
+            gradient="to top right, rgba(25,32,72,.7), rgba(25,32,72,.7)"
+          >
+            <v-img src="../assets/pic/logo.png" class="ma-5"></v-img>
+            <v-row justify="center">
+              <v-col cols="4">
+                <v-card color="black" class="pa-3">
+                  <v-img src="../assets/icon-one@2x.png" aspect-ratio="1" contain></v-img>
+                </v-card>
+              </v-col>
+              <v-col cols="4">
+                <v-card color="black" class="pa-1">
+                  <v-img src="../assets/icon-people@2x.png" aspect-ratio="1" contain></v-img>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
